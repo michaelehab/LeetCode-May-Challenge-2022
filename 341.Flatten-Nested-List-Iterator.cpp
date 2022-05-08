a6699fbc5bf0db509477244c887113b1c79abc6c
Michael Ehab <michaelehab16@gmail.com>
2022-05-08 20:25:03 +0200

    Added Day 8 Solution

---
+++ 341.Flatten-Nested-List-Iterator.cpp
@@ -0,0 +1,48 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+class NestedIterator {
+    queue <int> q;
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        fillQueue(nestedList, q);
+    }
+    
+    void fillQueue(vector<NestedInteger> &nestedList, queue <int> &qu){
+        for (auto& ni : nestedList) {
+            if (ni.isInteger()) q.push(ni.getInteger());
+            else fillQueue(ni.getList(), qu);
+        }
+    }
+    
+    int next() {
+        int tmp = q.front();
+        q.pop();
+        return tmp;
+    }
+    
+    bool hasNext() {
+        return q.size();
+    }
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
