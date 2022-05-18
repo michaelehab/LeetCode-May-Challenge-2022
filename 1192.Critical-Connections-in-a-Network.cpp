f44aec4664ababaf4a6a85e6dc19da1f6cb95a4e
Michael Ehab <michaelehab16@gmail.com>
2022-05-18 21:30:24 +0200

    Added Day 18 Solution

---
+++ 1192.Critical-Connections-in-a-Network.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+	vector<vector<int>> graph;
+
+	vector<int> firstTime, minTime, visited;
+	int time = 1;
+
+	void dfs(int node, int parent = -1) {
+		firstTime[node] = minTime[node] = time++;
+		visited[node] = true;
+
+		for(auto& child : graph[node]) {
+			if(child == parent) continue;
+			if(!visited[child]) dfs(child, node);
+			minTime[node] = min(minTime[child], minTime[node]);
+			if(firstTime[node] < minTime[child]) ans.push_back({node, child});
+		}
+	}
+
+	vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+		firstTime.resize(n), minTime.resize(n), visited.resize(n), graph.resize(n);
+		for(auto& edge : connections) {
+			int a = edge[0], b = edge[1];
+			graph[a].push_back(b);
+			graph[b].push_back(a);
+		}
+        
+		dfs(0);
+		return ans;
+	}
+};
