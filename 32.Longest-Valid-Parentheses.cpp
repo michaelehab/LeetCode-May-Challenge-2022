c62a06143f59ebe91a71ec721ea318150796ec91
Michael Ehab <michaelehab16@gmail.com>
2022-05-24 12:50:34 +0200

    Added Day 24 Solution

---
+++ 32.Longest-Valid-Parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> open;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(') open.push(i);
+            else if(open.size()) {
+                // Mark them as Valid
+                s[open.top()] = s[i] = 'V';
+                open.pop();
+            }
+        }
+
+        int curr = 0, ans = 0;
+        for(int i = 0; i <= s.size(); i++) {
+            // Finding longest lrngth of a Valid substring
+            if(s[i] == 'V') curr++;
+            else {
+                ans = max(ans, curr);
+                curr = 0;
+            }
+        }
+
+        return max(curr, ans);
+    }
+};
