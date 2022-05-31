630a875c4be57b4f6260e41877ae6834051a829e
Michael Ehab <michaelehab16@gmail.com>
2022-05-31 15:46:47 +0200

    Added Day 31 Solution

---
+++ 1461.Check-If-a-String-Contains-All-Binary-Codes-of-Size-K.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        if (k > s.length()) return false;
+        unordered_set<string> st;
+        for (int i = 0; i <= s.length() - k; ++i) st.insert(s.substr(i, k));
+        return (st.size() == pow(2, k));
+    }
+};
